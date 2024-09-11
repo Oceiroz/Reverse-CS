1f5a20c9e916de6c0b908017d1173b8e694dcd74
Lucas <115151663+Oceiroz@users.noreply.github.com>
2024-09-11 12:39:26 +0100

    Add files via upload

---
+++ Reverse String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetInput("write sumn cool");
+        }
+        static void GetInput(string inputMessage)
+        {
+            Console.WriteLine($"{inputMessage}");
+            var rawInput = Console.ReadLine();
+            char[] inputList = rawInput.ToCharArray();
+            Array.Reverse(inputList);
+            string inputReverse = new string(inputList);
+            Console.WriteLine(inputReverse);
+        }
+    }
+}
